961753b37296c555a329a23e3446f410706227ab
nndd91 <nndd91@hotmail.com>
2017-04-26 03:02:04 +0800

    Updated challenge 312

--- Rubychallenge/challenge312easy - Leet.rb
+++ Rubychallenge/challenge312easy - Leet.rb
@@ -20,25 +20,22 @@ W -> `//
 def l33t_this(string)
     array = string.upcase.chars
     #p array
-    conversions = { "A" => "4", "B" => "6", "E" => "3", "I" => "1", "L"=> "1", "M" =>"(V)", "N" => "(\)", "O"=> "0", "S"=> "5", "T" => "7", "V" => "\\/", "W" => "`//" }
+    conversions = { "A" => "4", "B" => "6", "E" => "3", "I" => "1", "L"=> "1", "M" =>"(V)", "N" => "(\\)", "O"=> "0", "S"=> "5", "T" => "7", "V" => "\\/", "W" => "`//" }
     #p conversions
     conversions2 = conversions.invert
-    p conversions2
+    #p conversions2
 
   if /[0-9`()\/]/.match(string)
     #convert from l33t
-    puts "Converting from l33t"
+    #puts "Converting from l33t"
     (0..array.length).each do |i|
-      temp = array[i]
       if array[i] == "`" && array[i+1] == "/"
         array[i] = "W"
         array[i+1] = ""
         array[i+2] = ""
-        puts "Changing #{temp} to #{array[i]}"
       elsif array[i] == "\\" && array[i+1] == "/"
         array[i] = "V"
         array[i+1] = ""
-        puts "Changing #{temp} to #{array[i]}"
       elsif array[i] == "(" && array[i+1] == "V"
         array[i] = "M"
         array[i+1] = ""
@@ -46,6 +43,7 @@ def l33t_this(string)
       elsif array[i] == "(" && array[i+1] == "\\"
         array[i] = "N"
         array[i+1] = ""
+        array[i+2] = ""
       else
         if conversions2.has_key?(array[i])
           array[i] = conversions2[array[i]]
@@ -53,7 +51,7 @@ def l33t_this(string)
       end
 
     end
-    p array.join
+    puts array.join
 
   else
     #convert to L33t
@@ -75,7 +73,11 @@ l33t_this("wow")
 l33t_this("41PH4")
 l33t_this("`//0`//")
 
-
+l33t_this("I am elite.")
+l33t_this("Da pain!")
+l33t_this("Eye need help!")
+l33t_this("3Y3 (\\)33d j00 t0 g37 d4 d0c70r.")
+l33t_this("1 n33d m4 p1llz!")
 
 
 
